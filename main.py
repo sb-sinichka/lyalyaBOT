22593114a5c6373c95d0e3b776ce2987975a370b
sb-sinichka <p81951554@gmail.com>
2026-03-29 18:19:30 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -26,8 +26,11 @@ def send_pass(message):
     else:
         bot.send_message(message.chat.id, 'нужно указать длину пароля')
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.send_message(message.chat.id, message.text)
+    
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
 
 bot.polling()
